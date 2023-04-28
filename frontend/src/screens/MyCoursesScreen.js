694b6fbb1bfd622c86aaa1c98b51dd17854a60da
unknown <sh.rehman222@gmail.com>
2023-04-28 16:14:55 +0500

    asdsa

--- frontend/src/screens/MyCoursesScreen.js
+++ frontend/src/screens/MyCoursesScreen.js
@@ -1,48 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 const MyCoursesScreen = () => {
-    return (
-        <div className="flex w-full h-screen overflow-auto">
-            <div className="flex flex-col w-full mx-auto h-full">
+  return (
+    <div className='flex w-full h-screen overflow-auto'>
+      <div className='flex flex-col w-full mx-auto h-full'>
+        <div className='w-full m-auto h-full p-5'>
+          <h1 className='font-bold text-base3'>My Courses</h1>
 
-                <div className="w-full m-auto h-full p-5">
-
-                    <h1 className='font-bold text-base3'>My Courses</h1>
-
-                    {/* map function here */}
-                    <div
-                        className={`flex shadow-4xl bg-white my-3 rounded-sm p-3 cursor-pointer border`}
-                    >
-                        <div className="flex flex-col items-cente gap-4 w-full px-4 py-2">
-                            <div className='w-full flex flex-col gap-2'>
-
-                                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
-                                    <p className='text-base2 font-semibold'>Course Title:</p>
-                                    <p className='text-base3 font-bold'>{`Algorithms`}</p>
-                                </div>
-                                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
-                                    <p className='text-base2 font-semibold'>instructor:</p>
-                                    <p className='text-base3 font-bold'>{`Ali murtaza`}</p>
-                                </div>
-                                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
-                                    <p className='text-base2 font-semibold'>Credit Hours:</p>
-                                    <p className='text-base3 font-bold'>{`3`}</p>
-                                </div>
-                                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
-                                    <p className='text-base2 font-semibold'>Course Code:</p>
-                                    <p className='text-base3 font-bold'>{`12345`}</p>
-                                </div>
-                            </div>
-
-                            <button className='bg-primary w-20 ml-auto rounded-[20px] p-1 text-white font-bold text-base2'>Edit</button>
-                        </div>
-                    </div>
-
+          {/* map function here */}
+          <div
+            className={`flex shadow-4xl bg-white my-3 rounded-sm p-3 cursor-pointer border`}
+          >
+            <div className='flex flex-col items-cente gap-4 w-full px-4 py-2'>
+              <div className='w-full flex flex-col gap-2'>
+                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
+                  <p className='text-base2 font-semibold'>Course Title:</p>
+                  <p className='text-base3 font-bold'>{`Algorithms`}</p>
                 </div>
+                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
+                  <p className='text-base2 font-semibold'>instructor:</p>
+                  <p className='text-base3 font-bold'>{`Ali murtaza`}</p>
+                </div>
+                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
+                  <p className='text-base2 font-semibold'>Credit Hours:</p>
+                  <p className='text-base3 font-bold'>{`3`}</p>
+                </div>
+                <div className='flex items-center justify-between py-1 border-b border-lightborder w-full pr-5'>
+                  <p className='text-base2 font-semibold'>Course Code:</p>
+                  <p className='text-base3 font-bold'>{`12345`}</p>
+                </div>
+              </div>
 
+              <button className='bg-primary w-20 ml-auto rounded-[20px] p-1 text-white font-bold text-base2'>
+                Edit
+              </button>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default MyCoursesScreen
--- frontend/src/screens/StudentDashboardScreen.js
+++ frontend/src/screens/StudentDashboardScreen.js
@@ -1,48 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import CourseCard from '../components/student/CourseCard'
 import AssignmentCard from '../components/student/AssignmentCard'
+import request from '../utils/request'
 
 const StudentDashboardScreen = () => {
-    return (
-        <div className="flex flex-col h-screen w-full p-5">
-            {/* <Header /> */}
-            <div className="flex gap-5 items-center justify-center h-full w-full">
+  const [courses, setCourses] = useState([])
+  const getAllCourses = async () => {
+    const { data } = await request.get('/courses')
+    setCourses(data.courses)
+  }
+  useEffect(() => {
+    getAllCourses()
+  }, [])
 
-                {/* course section */}
-                <div className=" bg-white w-full basis-[55%] border border-lightborder rounded p-4 h-full">
+  return (
+    <div className='flex flex-col h-screen w-full p-5'>
+      {/* <Header /> */}
+      <div className='flex gap-5 items-center justify-center h-full w-full'>
+        {/* course section */}
+        <div className=' bg-white w-full basis-[55%] border border-lightborder rounded p-4 h-full'>
+          {/* Heading */}
+          <h1 className='text-base3 mx-auto p-2 border-b-2'>Courses</h1>
 
-                    {/* Heading */}
-                    <h1 className='text-base3 mx-auto p-2 border-b-2'>Courses</h1>
+          <div className='flex flex-col gap-2 items-center justify-start overflow-auto p-5 w-full h-fit'>
+            {/* map func here */}
 
-                    <div className="flex flex-col gap-2 items-center justify-start overflow-auto p-5 w-full h-fit">
-                        {/* map func here */}
-
-                        {/* Course card */}
-                        <CourseCard />
-                    </div>
-                </div>
-
-                {/* Assignment section */}
-                <div className=" bg-white w-full basis-[45%] border border-lightborder rounded p-4 h-full">
-                    {/* heading */}
-                    <h1 className='text-base3 mx-auto p-2 border-b-2'> Assignments</h1>
-                    <div className="flex flex-col gap-2 items-center justify-start overflow-auto p-5 w-full h-fit">
-                        {/* map func here */}
-
-                        {/* Assignment card */}
-                        <AssignmentCard />
-                    </div>
-                </div>
-                {/* Assignment Card */}
-
-
-            </div>
-
-
+            {/* Course card */}
+            {courses.map((course, index) => {
+              return (
+                <CourseCard
+                  courseName={course.courseName}
+                  _id={course._id}
+                  courseCode={course.courseCode}
+                />
+              )
+            })}
+          </div>
         </div>
 
-    )
+        {/* Assignment section */}
+        <div className=' bg-white w-full basis-[45%] border border-lightborder rounded p-4 h-full'>
+          {/* heading */}
+          <h1 className='text-base3 mx-auto p-2 border-b-2'> Assignments</h1>
+          <div className='flex flex-col gap-2 items-center justify-start overflow-auto p-5 w-full h-fit'>
+            {/* map func here */}
+
+            {/* Assignment card */}
+            <AssignmentCard />
+          </div>
+        </div>
+        {/* Assignment Card */}
+      </div>
+    </div>
+  )
 }
 
 export default StudentDashboardScreen
