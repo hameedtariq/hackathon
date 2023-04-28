5d70b5627ef3696d0b8a5c528cc068e6a5e100e2
unknown <sh.rehman222@gmail.com>
2023-04-28 15:39:33 +0500

    Updated

--- frontend/src/store/slices/studentSlices.js
+++ frontend/src/store/slices/studentSlices.js
@@ -1,82 +1,82 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { login, studentSignup } from "../thunks/studentThunks";
+import { createSlice } from '@reduxjs/toolkit'
+import { login, studentSignup } from '../thunks/studentThunks'
 
 const loginSlice = createSlice({
-    name: "login",
-    initialState: {
-        loading: false,
-        Info: null,
-        error: "",
-        token: "",
+  name: 'login',
+  initialState: {
+    loading: false,
+    Info: null,
+    error: '',
+    token: '',
+  },
+  reducers: {
+    removeError: (state) => {
+      state.error = ''
     },
-    reducers: {
-        removeError: (state) => {
-            state.error = "";
-        },
-        logout: (state) => {
-            state.adminInfo = null;
-            localStorage.removeItem("Info");
-            localStorage.clear();
-        },
-        loginReset: (state) => {
-            state.loading = false;
-            state.success = false;
-            state.message = "";
-            state.error = "";
-        },
+    logout: (state) => {
+      state.adminInfo = null
+      localStorage.removeItem('Info')
+      localStorage.clear()
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(login.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(login.fulfilled, (state, action) => {
-                state.loading = false;
-                state.adminInfo = action.payload.user;
-                state.token = action.payload.token;
-            })
-            .addCase(login.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload?.error;
-            });
+    loginReset: (state) => {
+      state.loading = false
+      state.success = false
+      state.message = ''
+      state.error = ''
     },
-});
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false
+        state.Info = action.payload.user
+        state.token = action.payload.token
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload?.error
+      })
+  },
+})
 
 const studentSignupSlice = createSlice({
-    name: "signup",
-    initialState: {
-        loading: false,
-        success: false,
-        message: "",
-        error: "",
+  name: 'signup',
+  initialState: {
+    loading: false,
+    success: false,
+    message: '',
+    error: '',
+  },
+  reducers: {
+    signupReset: (state) => {
+      state.loading = false
+      state.success = false
+      state.message = ''
+      state.error = ''
     },
-    reducers: {
-        signupReset: (state) => {
-            state.loading = false;
-            state.success = false;
-            state.message = "";
-            state.error = "";
-        },
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(studentSignup.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(studentSignup.fulfilled, (state, action) => {
-                state.loading = false;
-                state.success = action.payload.success;
-                state.message = action.payload.message;
-            })
-            .addCase(studentSignup.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload?.error;
-            });
-    },
-});
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(studentSignup.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(studentSignup.fulfilled, (state, action) => {
+        state.loading = false
+        state.success = action.payload.success
+        state.message = action.payload.message
+      })
+      .addCase(studentSignup.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload?.error
+      })
+  },
+})
 
 // Actions
-export const {loginReset, logout} = loginSlice.actions
+export const { loginReset, logout } = loginSlice.actions
 
 // Reducers
-export { loginSlice, studentSignupSlice, }
+export { loginSlice, studentSignupSlice }
